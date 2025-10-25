34645e2cf82ec85ed1e406c3fe5593cf0932a74f
a01572343 <a01572343@tec.mx>
2025-10-25 17:19:24 -0600

    Update API URL

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 # URL de nuestro Servidor MCP
-MCP_API_URL = "http://127.0.0.1:8000"
+MCP_API_URL = "https://mi-mcp-server.onrender.com/"
 
 # --- Funciones para llamar a la API ---
 
